c4e272daa0fb16afb4f9c725564260c08c78d893
Hayk <haykghazaryan98@list.ru>
2018-09-08 15:23:35 +0400

    First commit

---
+++ ToDoApp/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  ToDoApp
+//
+//  Created by Hayk Ghazaryan on 08.09.18.
+//  Copyright © 2018 Hayk Ghazaryan. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
